39bd79578f7f269d92a917ebc42a40ca1de1a427
Cooper Pellaton <cooper@cooperpellaton.com>
2017-10-24 23:30:32 -0400

    Use a linter. It's good for you.

--- app.py
+++ app.py
@@ -11,50 +11,53 @@ e = create_engine('sqlite:///sightings.db')
 app = Flask(__name__)
 api = Api(app)
 
-# The columns in the database & our fields. 
+# The columns in the database & our fields.
 FIELDS = ["ferry_direction", "facility_type", "city", "resolution", "complaint_type", "intersection_street_1",
-   "cross_street_2", "agency_name", "taxi_company", "longitude", "vehicle_type", "school_number",
-   "street_name", "descriptor", "bridge_highway_segment", "agency", "school_or_citywide_complaint",
-   "garage_lot_name", "cross_street_1", "community_board", "closed_date", "school_not_found", 
-   "park_borough", "location", "school_city", "due_date", "x_coord", "school_region", "created_date",
-   "borough", "taxi_pick_up_location", "address_type", "intersection_street_2", "ferry_terminal_name",
-   "road_ramp", "park_facility_name", "bridge_highway_name", "school_state", "school_zip", "school_address",
-   "school_name", "latitude", "school_code", "incident_address", "location_type", "landmark", "y_coord",
-   "unique_key", "bridge_highway_direction", "status", "incident_zip", "school_phone_number"]
+          "cross_street_2", "agency_name", "taxi_company", "longitude", "vehicle_type", "school_number",
+          "street_name", "descriptor", "bridge_highway_segment", "agency", "school_or_citywide_complaint",
+          "garage_lot_name", "cross_street_1", "community_board", "closed_date", "school_not_found",
+          "park_borough", "location", "school_city", "due_date", "x_coord", "school_region", "created_date",
+          "borough", "taxi_pick_up_location", "address_type", "intersection_street_2", "ferry_terminal_name",
+          "road_ramp", "park_facility_name", "bridge_highway_name", "school_state", "school_zip", "school_address",
+          "school_name", "latitude", "school_code", "incident_address", "location_type", "landmark", "y_coord",
+          "unique_key", "bridge_highway_direction", "status", "incident_zip", "school_phone_number"]
 
 
 def jsonify_row(query):
-	""""Create a dict with the object names + objects for a given SQLite query."""
+    """Create a dict with the object names + objects for a given SQLite query."""
     return [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
 
 
 class Sightings(Resource):
+
     def get(self):
         """Return all rat sightings."""
         conn = e.connect()
         return {'sightings': jsonify_row(conn.execute("select * from sightings"))}
 
     def post(self):
-        """Execute an INSERT INTO sightings based on request JSON."""
+        # Execute an INSERT INTO sightings based on request JSON.
         conn = e.connect()
-        INSERT_SQL = f"""INSERT INTO sightings({','.join(FIELDS)}) VALUES({','.join("?"*len(FIELDS)})"""
+        INSERT_SQL = f"""INSERT INTO sightings({','.join(FIELDS)}) VALUES({','.join("?"*len(FIELDS))})"""
         query = conn.execute(INSERT_SQL, request.json)
 
     def delete(self):
-        """Execute a DELETE based on a given unique key."""
+        # Execute a DELETE based on a given unique key.
         conn = e.connect()
         unique_key = request.json["Unique Key"]
         try:
-            query =  conn.execute("DELETE FROM SIGHTINGS WHERE Name=?", (unique_key,))
-            return {'status':'success'}
+            query = conn.execute(
+                "DELETE FROM SIGHTINGS WHERE Name=?", (unique_key,))
+            return {'status': 'success'}
         except:
-            return {'status':'failure'}
-       
+            return {'status': 'failure'}
+
 
 class Sightings_50(Resource):
-	"""A special class just to get the first 50 sightings."""
+    # A special class just to get the first 50 sightings.
+
     def get(self):
-        """Return the first 50 sightings of rats."""
+        # Return the first 50 sightings of rats.
         conn = e.connect()
         return {'sightings': jsonify_row(conn.execute("select * from sightings LIMIT 50"))}
 
